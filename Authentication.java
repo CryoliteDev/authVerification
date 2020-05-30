2d5e8c1a86f7d687f85b57367d24fc6eb45a4440
Xiao Nai <50350070+XiaoNai43@users.noreply.github.com>
2020-05-30 18:44:12 -0400

    initial commit

---
+++ Authentication.java
@@ -0,0 +1,17 @@
+package authentication;
+
+/**
+ *
+ * @author Xiao
+ */
+public class Authentication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+        
+    }
+    
+}
